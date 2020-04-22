2f090a33786755bfc7561e73f2d9c7366778158f
Dusty <dustymccord@gmail.com>
2020-04-22 11:15:28 -0700

    Add Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <div class="ui inverted segment">
+        <div class="ui inverted secondary pointing menu">
+          <a class="active item" href="/">
+            Mind Master
+          </a>
+          <a class="item" href="/">
+            About Us
+          </a>
+          <a class="item" href="/">
+            Friend Finder
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
 export default Header;
